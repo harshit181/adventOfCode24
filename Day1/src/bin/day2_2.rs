a4847f9a898b2a6fd4c8a00338eadee75ebdcceb
harshit <harshit181@gmail.com>
2024-12-11 20:45:38 +0530

    Day 2 part2

---
+++ Day1/src/bin/day2_2.rs
@@ -0,0 +1,70 @@
+use std::io::stdin;
+
+fn main() {
+    let mut s=String::new();
+    let mut sum=0;
+
+    loop {
+        stdin().read_line(&mut s).unwrap();
+        if s.trim().is_empty(){
+            break;
+        }
+       let mut dd:Vec<&str>= s.split_whitespace().collect();
+        let is_safe = is_safe(&dd);
+        if is_safe{
+            sum=sum+1;
+        }
+        else{
+
+            for i in 0..dd.len(){
+                let mut dd2=dd.clone();
+                dd2.remove(i);
+                let is_safe = crate::is_safe(&dd2);
+                if is_safe{
+                    sum=sum+1;
+                    break;
+                }
+            }
+
+
+        }
+
+
+        s="".to_string();
+    }
+
+    println!("{}",sum);
+}
+
+fn is_safe(dd: &Vec<&str>) -> bool {
+    let mut is_safe = true;
+    let mut is_incr = -2;
+    let ss: Vec<i32> = dd.iter().map(|x| x.parse::<i32>().unwrap()).collect();
+    let mut prev = -1;
+    for asd in ss {
+        if prev != -1 {
+            let sss = asd - prev;
+            if sss.abs() > 3 || sss.abs() < 1 {
+                is_safe = false;
+                break;
+            }
+
+            if is_incr == -2 {
+                if sss == 0 {
+                    is_safe = false;
+                    break;
+                }
+                is_incr = sss / (sss.abs());
+            } else {
+                let check_is_incr = sss / (sss.abs());
+                if check_is_incr != is_incr {
+                    is_safe = false;
+                    break;
+                }
+            }
+        }
+
+        prev = asd;
+    }
+    is_safe
+}
